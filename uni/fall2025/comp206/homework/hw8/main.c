b0ff210e395305ddf08e076e5fb31588bad8496e
mingli202 <vincentmingli@gmail.com>
2025-11-07 19:26:02 -0500

    finished homework 8

--- uni/fall2025/comp206/homework/hw8/main.c
+++ uni/fall2025/comp206/homework/hw8/main.c
@@ -2,8 +2,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s N PATH\n", argv[0]);
+    return 1;
+  }
 
+  int n = atoi(argv[1]);
+  if (n <= 0) {
+    fprintf(stderr, "N must be a positive integer\n");
+    return 1;
+  }
+
+  char *path = argv[2];
+
+  struct ringbuffer rb = rb_init(n);
+
+  FILE *fp = fopen(path, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Failed to open %s\n", path);
+    return 3;
+  }
+
+  char line[LINE_LENGTH + 1];
+  while (fgets(line, LINE_LENGTH, fp) != NULL) {
+    rb_push(&rb, line);
+  }
+  fclose(fp);
+
+  while (rb_pop(&rb, line)) {
+    printf("%s", line);
+  }
+
+  rb_destroy(&rb);
+
+  return 0;
 }
