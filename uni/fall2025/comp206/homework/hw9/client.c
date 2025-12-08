7a78e914d8698b7854a43a073c95bf03b9121a04
mingli202 <vincentmingli@gmail.com>
2025-12-08 12:04:08 -0500

    finished hw9

--- uni/fall2025/comp206/homework/hw9/client.c
+++ uni/fall2025/comp206/homework/hw9/client.c
@@ -1,66 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-    struct sockaddr_in address;
-    int sock_fd;
-    char buf[1024];
+int main(int argc, char *argv[]) {
+  struct sockaddr_in address;
+  int sock_fd;
+  char buf[1024];
 
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd < 0) {
-        perror("socket");
-        return 1;
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_fd < 0) {
+    perror("socket");
+    return 1;
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1 aka localhost
+  address.sin_port = htons(3000);
+  // ^ to avoid conflicts with others, you should change the port number to
+  // something else. Reflect that change in server.c
+
+  if (-1 == connect(sock_fd, (struct sockaddr *)&address, sizeof(address))) {
+    perror("connect");
+    return 1;
+  }
+
+  // make stdin nonblocking:
+  fcntl(0, F_SETFL, O_NONBLOCK);
+
+  // make the socket nonblocking:
+  fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+
+  FILE *server = fdopen(sock_fd, "r+");
+
+  while (1) {
+    // Complete the below polling loop:
+    // - try to read from stdin
+    //   - if that succeeds: print the data to the socket
+    //   - if reading from stdin fails for any reason other than blocking,
+    //     close the socket and exit the program with code 0
+    // - try to read from the socket, and forward to stdout
+    //   - if that succeeds: print the data to stdout
+    //   - if it fails for any reason other than blocking,
+    //     close the socket and exit the program with code 0
+
+    if (NULL == fgets(buf, sizeof(buf), stdin)) {
+      if (errno != EAGAIN || errno != EWOULDBLOCK) {
+        perror("fgets");
+        fclose(server);
+        return 0;
+      }
+    } else {
+      fprintf(server, "%s", buf);
+      fflush(stdin);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1 aka localhost
-    address.sin_port = htons(7878);
-    // ^ to avoid conflicts with others, you should change the port number to
-    // something else. Reflect that change in server.c
-
-    if (-1 == connect(sock_fd, (struct sockaddr *)&address, sizeof(address))) {
-        perror("connect");
-        return 1;
+    if (NULL == fgets(buf, sizeof(buf), server)) {
+      if (errno != EAGAIN || errno != EWOULDBLOCK) {
+        perror("fgets");
+        fclose(server);
+        return 0;
+      }
+    } else {
+      printf("%s", buf);
     }
 
-    // make stdin nonblocking:
+    usleep(100 * 1000); // wait 100ms before checking again
+  }
 
-
-
-
-    // make the socket nonblocking:
-
-
-
-
-
-    FILE *server = fdopen(sock_fd, "r+");
-
-    while (1) {
-        // Complete the below polling loop:
-        // - try to read from stdin
-	//   - if that succeeds: print the data to the socket
-	//   - if reading from stdin fails for any reason other than blocking,
-	//     close the socket and exit the program with code 0
-        // - try to read from the socket, and forward to stdout
-	//   - if that succeeds: print the data to stdout
-	//   - if it fails for any reason other than blocking,
-	//     close the socket and exit the program with code 0
-
-
-
-
-
-
-
-        usleep(100 * 1000); // wait 100ms before checking again
-    }
-
-    return 0;
+  return 0;
 }
