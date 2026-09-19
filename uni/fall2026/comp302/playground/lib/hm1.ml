c1af857fe68549bc86a5ce62cbf634b8c69b0404
mingli202 <vincentmingli@gmail.com>
2026-09-19 15:43:42 -0400

    added doc strings to hm1

--- uni/fall2026/comp302/playground/lib/hm1.ml
+++ uni/fall2026/comp302/playground/lib/hm1.ml
@@ -8,15 +8,20 @@ let distance_tests =
       (* output: the distance between (0,0) and (0,0) is 0 *) );
     (* end each case with a semicolon *)
     (* Your test cases go here *)
+    (* happy path *)
     (((2, 4), (5, 6)), 5);
+    (* negative numbers and negative distance if no abs *)
     (((-3, -5), (-1, -2)), 5);
+    (* symmetry with the above statement *)
     (((-1, -2), (-3, -5)), 5);
   ]
 
-(* TODO: Correct this implementation so that it compiles and returns
-         the correct answers.
-*)
-let distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+(** returns the Manhattan distance between the two given points
+    @param (x1,y1) the first point
+    @param (x2,y2) the second point
+    @return the Manhattan distance*)
+let distance ((x1, y1) : int * int) ((x2, y2) : int * int) : int =
+  abs (x1 - x2) + abs (y1 - y2)
 
 (* Question 2: Binomial *)
 (* TODO: Write your own tests for the binomial function.
@@ -34,10 +39,13 @@ let binomial_tests =
     ((10, 7), 120);
   ]
 
-(* TODO: Correct this implementation so that it compiles and returns
-         the correct answers.
+(** implements the binomial operation n choose k
+    @param n number of objects
+    @param k sample size
+    @return
+      the number of possible combinasion of size []k to choose from [n] objects
 *)
-let binomial n k =
+let binomial (n : int) (k : int) : int =
   if n < k then 0
   else
     let factorial (n : int) =
@@ -53,8 +61,12 @@ let binomial n k =
 (* TODO: Write a good set of tests for lucas_tests. *)
 let lucas_tests = [ (0, 2); (1, 1); (2, 3); (3, 4); (4, 7) ]
 
-(* TODO: Implement a tail-recursive helper lucas_helper. *)
+(** tail recursive helper to compute the lucas number sequence
+    @param n the number of remaining calls to [lucas_helper]
+    @param a the (n - 1)th number in the sequence
+    @param b the nth number in the sequence
+    @return the nth number in the sequence *)
 let rec lucas_helper n a b = if n = 0 then b else lucas_helper (n - 1) b (a + b)
 
-(* TODO: Implement lucas by calling lucas_helper. *)
+(** returns the nth number in the lucas number sequence *)
 let lucas n = if n = 0 then 2 else if n = 1 then 1 else lucas_helper (n - 1) 2 1
