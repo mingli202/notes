7f6b7aadbb7363be70d05a2a74d04243b4d9e5d4
mingli202 <vincentmingli@gmail.com>
2026-09-20 16:03:56 -0400

    parse_number

--- uni/fall2026/comp302/playground/lib/hm2.ml
+++ uni/fall2026/comp302/playground/lib/hm2.ml
@@ -4,6 +4,9 @@ exception Invalid_test_case
 (** The exception raised when a test case has an input outside the domain of the
     tested function. *)
 
+exception Invalid_token of string
+exception Invalid_exp of string
+
 type nat = Z | S of nat
 
 type exp =
@@ -67,7 +70,106 @@ let rec q1c_add (n : nat) (m : nat) : nat =
 
 (* Question 2 *)
 
-(* TODO: Implement {!q2a_neg}. *)
+(** split the given string into a list of char *)
+let string_split (s : string) : char list =
+  let rec string_split_h (s : string) (i : int) =
+    if i = String.length s then [] else s.[0] :: string_split_h s (i + 1)
+  in
+  string_split_h s 0
+
+let rec parse_eq_list (s : char list) (acc : exp) : exp =
+  match s.[i] with
+  | '(' -> parse_eq_list s (i + 1)
+  | ')' -> Var
+  | 'x' -> Var
+  | '*' -> Times (Var, Var)
+  | '/' -> Times (Var, Var)
+  | '+' -> Plus (Var, Var)
+  | '-' -> Minus (Var, Var)
+  | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> ""
+  | ' ' -> parse_eq_list s (i + 1)
+  | _ -> raise (Invalid_token "unknown token")
+
+(** Finds a string of numbers and return the rest of the string to parse *)
+let rec find_number (s : char list) : string * char list =
+  match s with
+  | (('0' .. '9' | '.') as c) :: rest ->
+      let rest_of_n, final_rest = find_number rest in
+      (String.of_char c ^ rest_of_n, final_rest)
+  | _ -> ("", s)
+
+(** finds the number and return the rest of the string to parse
+
+    Example:
+    {[
+    (* (Const 123.1) [] *)
+    parse_number "123.1";
+
+    (* (Const 123.1) [" ", "+", " ", "x"] *)
+    parse_number "123.1 + x"
+    ]} *)
+let parse_number (s : char list) : exp * char list =
+  let str_n, rest = find_number s in
+  (Const (float_of_string str_n), rest)
+
+(** parse the first token because the actual parsing relies on a previous, which
+    at the start doesn't have any *)
+let rec parse_eq_first (s : char list) : exp =
+  match s with
+  | '(' :: rest -> parse_eq_first rest
+  | ')' :: rest -> raise (Invalid_token "cannot start with ')'")
+  | 'x' :: rest -> parse_eq_list rest Var
+  | '*' :: rest -> raise (Invalid_token "cannot start with '*'")
+  | '/' :: rest -> raise (Invalid_token "cannot start with '/'")
+  | '+' :: rest -> raise (Invalid_token "cannot start with '+'")
+  | '-' :: rest -> raise (Invalid_token "cannot start with '-'")
+  | ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '.') :: rest ->
+      let e, rest = parse_number s in
+      parse_eq_list rest e
+  | ' ' :: s -> parse_eq_first s
+  | _ -> raise (Invalid_token "unknown token")
+
+(** parse the equation string into an exp so I can feed it to tests without
+    having to write out the chain of exp
+
+    Example:
+    {[
+    (* Const 1 *)
+    parse_eq "1";
+
+    (* Const 12 *)
+    parse_eq "12";
+    parse_eq "12.";
+    parse_eq "12.0";
+
+    (* Const 12.5 *)
+    parse_eq "12.5";
+
+    (* Times (Const 10, Var) *)
+    parse_eq "10x";
+
+    (* Plus (Times (Const 10, Var), Const 5) *)
+    parse_eq "10x + 5";
+
+    (* Plus (Times (Const 10, Var), Const 5) *)
+    parse_eq "10(x + 5)";
+    parse_eq "10(x + 5";
+    parse_eq "10 * (x + 5)";
+    parse_eq "(2 + 8) * (x + 5)";
+    parse_eq "(2 * 5) * (x + 5)"
+    ]} *)
+let parse_eq (s : string) : exp = parse_eq_first (string_split s)
+
+(** negate the given expression e, but don't resolve the parathensis
+
+    Example:
+
+    {[
+    q2a_neg 1 (* -(1) *);
+    q2a_neg (2x + 1) (* 2x + 1 *);
+    q2a_neg (3x - 2 - 5 (x + 1))
+    (* -(3x - 2 - 5 (x + 1)) *)
+    ]} *)
 let q2a_neg (e : exp) : exp = raise Not_implemented
 
 (* TODO: Implement {!q2b_minus}. *)
